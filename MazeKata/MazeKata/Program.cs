64e591833f4de580e944088405ac578c8acf6225
Braden Alsford <braden.alsford@myob.com>
2020-03-20 15:15:36 +1100

    Commit from brad

--- MazeKata/MazeKata/Program.cs
+++ MazeKata/MazeKata/Program.cs
@@ -7,6 +7,7 @@ namespace MazeKata
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Braden was here.");
         }
     }
 }
